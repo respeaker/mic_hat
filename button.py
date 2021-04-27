7b046c2afc62c5bc1b40eda452886fca6164c208
AIWintermuteAI <dmitrywat@gmail.com>
2021-04-27 03:10:01 +0100

    added gitignore and button example

---
+++ button.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+BUTTON = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON, GPIO.IN)
+
+while True:
+    state = GPIO.input(BUTTON)
+    if state:
+        print("off")
+    else:
+        print("on")
+    time.sleep(1)
